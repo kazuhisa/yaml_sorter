c8e8891182898e66773e3cfff2e2beffc99be3ed
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2015-02-02 21:01:17 +0900

    add hash extender.

---
+++ config/initializers/hash_ex.rb
@@ -0,0 +1,12 @@
+# http://dan.doezema.com/2012/04/recursively-sort-ruby-hash-by-key/
+class Hash
+  def sort_by_key(recursive = false, &block)
+    self.keys.sort(&block).reduce({}) do |seed, key|
+      seed[key] = self[key]
+      if recursive && seed[key].is_a?(Hash)
+        seed[key] = seed[key].sort_by_key(true, &block)
+      end
+      seed
+    end
+  end
+end

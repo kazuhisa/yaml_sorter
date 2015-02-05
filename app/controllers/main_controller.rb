a5842e55982d94c6b6b1df42a813a21f0e8fce5d
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2015-02-05 21:40:10 +0900

    fix index visual.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,27 @@
 class MainController < ApplicationController
   def index
-    @text = 'aaa: 123'
+    @text = DEFAULT_YAML
   end
 
   def convert
     yaml = YAML.load(params[:content])
     sorter = Sorter.new(yaml)
     @text = sorter.sort
+  rescue Psych::SyntaxError => e
+    @error_message = e.message
+    @error = true
+    @text = params[:content]
+  ensure
     render action: :index
   end
+
+  DEFAULT_YAML = <<EOS
+---
+ccc: 789
+bbb: 456
+aaa: 123
+ddd:
+  fff: 999
+  eee: 888
+EOS
 end
